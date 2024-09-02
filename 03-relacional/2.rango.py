94eb8f99532112379c95871f2d7ca86d94d42cb5
jamesmosq <161092898+jamesmosq@users.noreply.github.com>
2024-09-02 18:16:23 -0500

    Delete 03-relacional directory

--- 03-relacional/2.rango.py
+++
@@ -1,6 +0,0 @@
-numero = int(input("Ingrese un número: "))
-
-if 10 <= numero <= 20:
-    print("El número está entre 10 y 20.")
-else:
-    print("El número está fuera del rango.")
--- 03-relacional/3.anioBisc.py
+++
@@ -1,6 +0,0 @@
-año = int(input("Ingrese un año: "))
-
-if (año % 4 == 0 and año % 100 != 0) or año % 400 == 0:
-    print("El año es bisiesto.")
-else:
-    print("El año no es bisiesto.")
