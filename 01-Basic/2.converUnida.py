4e56f4fd783524a80b0d6358abf2f7bad7fddc12
jamesmosq <161092898+jamesmosq@users.noreply.github.com>
2024-09-02 18:16:40 -0500

    Delete 01-Basic directory

--- 01-Basic/2.converUnida.py
+++
@@ -1,8 +0,0 @@
-metros = float(input("Ingrese la cantidad en metros: "))
-
-centimetros = metros * 100
-kilometros = metros / 1000
-
-print(metros, "metros equivalen a:")
-print(centimetros, "centímetros")
-print(kilometros, "kilómetros")
