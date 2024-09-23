f6bbb79bdcaa1e0a94c4eb77643b6c37b7422bff
jamesmosq <161092898+jamesmosq@users.noreply.github.com>
2024-09-23 18:14:25 -0500

    Delete Errors directory

--- Errors/5.py
+++
@@ -1,9 +0,0 @@
-#5.ValueError: Calculadora de edad con entrada inválida
-
-def calcular_edad():
-    año_nacimiento = input("Ingrese su año de nacimiento: ")
-    año_actual = 2023
-    edad = año_actual - int(año_nacimiento)
-    print(f"Tienes {edad} años.")
-
-calcular_edad()  # Error si el usuario ingresa algo que no sea un número
